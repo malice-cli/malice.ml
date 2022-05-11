26990f424287f11ef7cb11cb1573b1d870d1be7d
Malder <95049678+Meta-Lymph-Node@users.noreply.github.com>
2022-05-11 02:30:07 +0000

    ji

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-var bod = document.getElementById('malice');
+
+window.onload = function() {
+    var bod = document.getElementById('malice');
 bod.innerHTML = bod.innerHTML.replace('--[=[',"");
+new Audio('./LAUGH.mp3').play();
+setTimeout(()=>{
+  window.close();
+},500)
+}
