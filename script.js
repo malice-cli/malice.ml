8014d7904b4bee7a5a973ebc884c8306739b0520
Malder <95049678+Meta-Lymph-Node@users.noreply.github.com>
2022-05-26 00:06:02 +0000

    new encryp[tino

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-
 window.onload = function() {
     var bod = document.getElementById('malice');
 bod.innerHTML = bod.innerHTML.replace('--[=[',"");
 new Audio('./LAUGH.mp3').play();
 setTimeout(()=>{
-  window.close();
+  window.open('','_self').close()
 },500)
 }
