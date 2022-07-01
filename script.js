2111f607bc44c2827ace418d0abd4dd50b266a9e
m2 <95049678+malice-cli@users.noreply.github.com>
2022-07-01 01:13:41 +0000

    doi

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
 window.onload = function() {
-    var bod = document.getElementById('malice');
-bod.innerHTML = bod.innerHTML.replace('--[=[',"");
-new Audio('./LAUGH.mp3').play();
-setTimeout(()=>{
-  window.open('','_self').close()
-},500)
+document.location = "https://appl.malice.ml/DOINYOURMOMDOINDOINYOURMOMDOINDOINYOURMOMDOINDOINYOURMOMYOUKNOWWESTRAIGHTDOINYOURMOM"
 }
